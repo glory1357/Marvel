382f7cf8f577a5256492e5798f8b47caa09a5ef0
glory1357 <efim133@mail.ru>
2021-12-22 21:13:04 +0300

    new Project Marvel ref

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import MarvelService from '../../services/MarverService';
 import Spinner from '../spinner/Spinner';
@@ -6,7 +7,6 @@ import ErrorMessage from '../errorMessage/ErrorMessage';
 import Skeleton from '../skeleton/Skeleton';
 
 import './charInfo.scss';
-import thor from '../../resources/img/thor.jpeg';
 
 
 class CharInfo extends Component {
@@ -127,4 +127,7 @@ const View = ({char}) => {
     )
 }
 
+CharInfo.propTypes = {
+    charId: PropTypes.number
+}
 export default CharInfo;
